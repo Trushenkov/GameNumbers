12e7767937759938b578e767601cba83ae191f2b
Трушенков Дмитрий <diman28051999@gmail.com>
2017-09-19 14:20:56 +0300

    Initial commit

---
+++ src/ru/tds/game/GameLauncher.java
@@ -0,0 +1,8 @@
+package ru.tds.game;
+
+public class GameLauncher {
+    public static void main(String[] args) {
+        GuessGame game = new GuessGame();
+        game.startGame();
+    }
+}
---
+++ src/ru/tds/game/Player.java
@@ -0,0 +1,13 @@
+package ru.tds.game;
+
+public class Player {
+    int number = 0;
+
+    public void guess(){
+        number = (int) (Math.random() * 10);
+    }
+
+    public void writeNumber (){
+        System.out.println("Я предполагаю " + number);
+    }
+}
