781a21391dd85edb137ab2da9fba4163e792fc58
Трушенков Дмитрий <diman28051999@gmail.com>
2017-09-21 18:59:59 +0300

    Добавлены комментарии к методам и классу.

--- src/ru/tds/game/Player.java
+++ src/ru/tds/game/Player.java
@@ -1,14 +1,25 @@
 package ru.tds.game;
 
+/**
+ * Класс, в котором реализовано генерирование чисел для игроков.
+ *
+ * @author Trushenkov Dmitry 15IT18
+ */
 public class Player {
-    
+
     int number = 0;
 
-    public void guess(){
+    /**
+     * Метод для генерирования рандомного значения для переменой number.
+     */
+    public void guess() {
         number = (int) (Math.random() * 10);
     }
 
-    public void writeNumber (){
+    /**
+     * Метод для вывода предполагаемого числа.
+     */
+    public void writeNumber() {
         System.out.println("Я предполагаю " + number);
     }
 }
