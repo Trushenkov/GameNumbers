817923971e5294219517b78c95ee26b69b2409b5
Трушенков Дмитрий <diman28051999@gmail.com>
2017-09-21 19:01:09 +0300

    Добавлен комментарий к классу.

--- src/ru/tds/game/GameLauncher.java
+++ src/ru/tds/game/GameLauncher.java
@@ -1,5 +1,10 @@
 package ru.tds.game;
 
+/**
+ * Класс, в котором реализован запуск игры.
+ *
+ * @author Trushenkov Dmitry 15IT18
+ */
 public class GameLauncher {
 
     public static void main(String[] args) {
